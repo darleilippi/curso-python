cf198d0c4b7afcbde8766591475dcc37ebd2019f
Darlei A. Lippi <darleilippi@uri.edu.br>
2021-06-28 19:25:07 -0300

    Adição da aula 1 e do venv

---
+++ aula1/aula1.py
@@ -0,0 +1,8 @@
+# Comentários de código em Python
+# Isso é um comentário
+print("Hello World 1!")
+print("Hello World 2!")  # Deve-se ter dois espaços antes de um comentário em pinha PIP 8
+"""print("Hello World 3!")
+print("Hello World 4!")
+print("Hello World 5!")"""
+print("Hello World 6!")
